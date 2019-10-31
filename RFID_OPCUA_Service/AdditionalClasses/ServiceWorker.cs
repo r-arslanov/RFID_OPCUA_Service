be3444bdb098ba0617eb0a62fb6cf2f8783b1f58
Рустэм Арсланов <sobakasobaka6@gmail.com>
2019-10-31 15:46:08 +0500

    add RF680R class

---
+++ RFID_OPCUA_Service/AdditionalClasses/ServiceWorker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Opc.Ua.Client;
+using Siemens.UAClientHelper;
+using Opc.Ua;
+using RfidOpcUaForm;
+using System.Xml;
+
+namespace RFID_OPCUA_Service.AdditionalClasses
+{
+    class ServiceWorker
+    {
+        #region Fields
+        int cRFID;
+        RegistryKey rfidKey;
+        RegistryKey software;
+        RegistryKey configData;
+
+        List<RF680R> listRF;
+        #endregion
+
+        public ServiceWorker()
+        {
+            RegistryKey cu = Registry.CurrentUser;
+            software = cu.OpenSubKey("Software", true);
+            rfidKey = software.CreateSubKey("RFID", true);
+            configData = rfidKey.OpenSubKey("config", true);
+            listRF = new List<RF680R>();
+
+        }
+
+        public void init()
+        {
+            cRFID = (int)rfidKey.GetValue("num");
+            List<string> ip_list = new List<string>(configData.GetValue("ipPort") as string[]);
+
+            for(int i=0; i<ip_list.Count; i++)
+            {
+                listRF.Add(new RF680R(ip_list[i]));
+            }
+            
+            foreach(var rfid in listRF)
+            {
+                rfid.connectOPC(rfidKey);
+                rfid.startScan(rfidKey);
+            }
+        }
+
+        public void destroy()
+        {
+            foreach (var rfid in listRF)
+            {
+                rfid.stopScan(rfidKey);
+            }
+
+            rfidKey.Close();
+            software.Close();
+        }
+
+        
+
+    }
+
+}
--- RFID_OPCUA_Service/Service1.cs
+++ RFID_OPCUA_Service/Service1.cs
@@ -1,27 +1,32 @@
 ﻿using Microsoft.Win32;
 using System.ServiceProcess;
+using System.Threading;
+using RFID_OPCUA_Service.AdditionalClasses;
 
 namespace RFID_OPCUA_Service
 {
     public partial class Service1 : ServiceBase
     {
+        #region Fields
+        ServiceWorker worker;
+        #endregion
         public Service1()
         {
             InitializeComponent();
+            worker = new ServiceWorker();
         }
 
         protected override void OnStart(string[] args)
         {
-            RegistryKey cu = Registry.CurrentUser;
-            RegistryKey software = cu.OpenSubKey("Software", true);
-            RegistryKey rfidKey = software.CreateSubKey("RFID", true);
-            rfidKey.SetValue("testPar", "testVal123");
-            rfidKey.Close();
-            software.Close();
+            Thread tWorker = new Thread(worker.init);
+            tWorker.Start();
         }
 
         protected override void OnStop()
         {
+            worker.destroy();
         }
     }
+
+
 }
